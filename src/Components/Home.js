ef6a994cfc575789598f7e30daa3533505120634
Chetan Harjani <chetonix@Chetans-MacBook-Pro.local>
2022-07-17 13:20:44 +0530

    add fetch method to the useEffect hook to fetch movie list data and console log it

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function Home() {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+  fetch(`https://hoblist.com/api/movieList`)
+   .then((response) => console.log(response));
+ }, []);
+
+
   return (
     <>
       <h1>Login Successfully</h1>
