a2da74e44229e0df9a47d6c28f53362647dd4011
Chetan Harjani <chetonix@Chetans-MacBook-Pro.local>
2022-07-17 18:13:07 +0530

    add flexbox to home css

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -4,13 +4,14 @@ function MovieCard(props) {
 
     const {id, title, genre, director, stars, pageViews, language, totalVoted, releasedDate, poster, runTime} = props.info;
 
+
   return (
     <div className='card'>
         <img src={poster} alt="movie-poster" className='card__image'/>
         <div className="card__body">
             <h4>{title}</h4> 
             <p>Genre: {genre}</p>
-            <p>Director: </p>
+            <p>{director[0] ? `Director: ${director[0]}` : 'Director: '}</p>
             <p>Starring: </p>
             <p>{pageViews} | Voted by {totalVoted} people</p>
         </div>
